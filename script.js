83d24685aaaf6e81455a0bfefaed84747f60bfa5
Jay Shah <jayyshah69@gmail.com>
2023-12-31 20:04:03 -0500

    Created a function which decides who wins

--- script.js
+++ script.js
@@ -13,5 +13,30 @@ function getComputerChoice() {
 
 function play(playerSelection, computerSelection){
     /** There are 3*3= 9 outcomes so list them all one by one ig*/
-    if(playerSelection.toLowerCase() == computerSelection.toLowerCase()) return "Tie!"
+
+    if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissor"){
+        return "You win! Rock beats scissor"
+    }
+    if(playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase() == "paper"){
+        return "You win! Scissor beats Paper"
+    }
+    if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock"){
+        return "You win! Paper beats Rock"
+    }
+
+    if(playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase() == "rock"){
+        return "You lose! Rock beats scissor"
+    }
+    if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissor"){
+        return "You lose! Paper beats scissor"
+    }
+    if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper"){
+        return "You lose! Paper beats rock"
+    }
+
+    return "It's a tie"
+}
+
+function game() {
+    
 }
